7aae8264f76d74a9ca3852677c7d5e7f4cec1848
cjwinchester <cjwinchester@gmail.com>
2014-11-17 21:25:33 -0600

    extending

--- ne-county-choroplether.py
+++ ne-county-choroplether.py
@@ -4,23 +4,21 @@ from bs4 import BeautifulSoup
 counties = {}
 reader = csv.reader(open('governor_county_cleaned.csv'), delimiter=",")
 
+colors = ['#6baed6', '#c09853', '#fb6a4a']
+
 for row in reader:
     county = row[0].replace(" ","-").lower()
-    hassebrook = row[1]
-    elworth = row[2]
-    ricketts = row[3]
-    winner = max(int(hassebrook), int(elworth), int(ricketts))
-    if winner == int(hassebrook):
-        win_color = "#6baed6"
-    elif winner == int(ricketts):
-        win_color = "#fb6a4a"
-    elif winner == int(elworth):
-        win_color = "#c09853"
+    nums = []
+    for thing in row[1:]:
+        nums.append(int(thing))
+    winner = max(nums)
+    position = nums.index(winner)
+    win_color = colors[position]
     counties[county] = win_color
-
+    
 svg = open('ne-counties.svg', 'rb').read()
 
-path_style = "color:#aaa; stroke:#000; stroke-width:0.5; stroke-linecap:butt; stroke-linejoin:miter; stroke-miterlimit:4; fill:"
+path_style = "color:#aaa; stroke:#fff; stroke-width:1; stroke-linecap:butt; stroke-linejoin:miter; stroke-miterlimit:4; fill:"
 
 soup = BeautifulSoup(svg, "html.parser")
 
@@ -31,5 +29,3 @@ for p in paths:
     p['style'] = path_style + color
     
 print soup.prettify()
-
-# usage: python ne-county-choroplether.py > governor_results.svg
